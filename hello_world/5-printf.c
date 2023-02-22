25031540c914391f92ca801ed2999b3b9447b256
Amiineayari <6485@holbertonstudents.com>
2023-02-22 00:49:00 -0800

    C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,7 +4,7 @@ c program prints a simple massage to the console using the printf () function
 */
 int main(void)
 {
-printf("With proper grammar,but the outcome is a piece of art\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
 	 
