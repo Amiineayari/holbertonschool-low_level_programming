0d24c5ad271d49250fb5d1533d3e2c2a5d650668
Amiineayari <6485@holbertonstudents.com>
2023-02-26 12:41:25 -0800

    program that prints all possible combinations of single-digit numbers.

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -6,25 +6,17 @@
 */
 int main(void)
 {
-int i, j, k;
+int i, j;
 for (i = 0; i < 10; i++)
 {
 for (j = i + 1; j < 10; j++)
 {
-for (k = j + 1; k < 10; k++)
-{
 putchar(i + '0');
-putchar(',');
-putchar(' ');
 putchar(j + '0');
-putchar(',');
-putchar(' ');
-putchar(k + '0');
-if (i != 7 || j != 8 || k != 9)
+if (!(i == 8 && j == 9))
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 }
