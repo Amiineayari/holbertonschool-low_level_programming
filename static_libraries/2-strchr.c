2b824171990a3a819508721c576116d93389d2ae
Amiineayari <6485@holbertonstudents.com>
2023-05-04 14:23:01 -0700

    static librairies

--- static_libraries/2-strchr.c
+++ static_libraries/2-strchr.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
-*_strlen - returns length of a string.
-*@s: input
-*Return: Always 0.
-*/
-int _strlen(char *s)
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
 {
 int i = 0;
-while (*s != '\0')
+for (; s[i] >= '\0'; i++)
 {
-s++;
-i++;
+if (s[i] == c)
+return (&s[i]);
 }
-return (i);
+return (0);
 }
