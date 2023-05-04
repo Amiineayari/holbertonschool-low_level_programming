7d38ec39b8d8772d272ff6f85ddfa1a1719e0a8d
Amiineayari <6485@holbertonstudents.com>
2023-05-04 12:47:22 -0700

    .h

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -16,7 +16,7 @@ typedef struct hash_node_s
 char *key;
 char *value;
 struct hash_node_s *next;
-}hash_node_t;
+} hash_node_t;
 
 /**
  * struct hash_table_s - hash table DS.
@@ -27,7 +27,7 @@ typedef struct hash_table_s
 {
 unsigned long int size;
 hash_node_t **array;
-}hash_table_t;
+} hash_table_t;
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
@@ -50,7 +50,7 @@ char *value;
 struct shash_node_s *next;
 struct shash_node_s *sprev;
 struct shash_node_s *snext;
-}shash_node_t;
+} shash_node_t;
 /**
  * struct shash_table_s - sorted hash table ds.
  * @size: size of array.
@@ -64,7 +64,7 @@ unsigned long int size;
 shash_node_t **array;
 shash_node_t *shead;
 shash_node_t *stail;
-}shash_table_t;
+} shash_table_t;
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);
